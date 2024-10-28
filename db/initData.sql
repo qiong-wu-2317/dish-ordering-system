1c365b183e45a0036de8ed29510758916d9795b9
qiong-wu-2317 <wuqiong2317@gmail.com>
2024-10-27 22:26:48 -0700

    update db init data, add db file

--- db/initData.sql
+++ db/initData.sql
@@ -1,6 +1,5 @@
 .mode csv
 .import test_data/User_Customer.csv User
-.import test_data/User_Restaurant.csv User
 .import test_data/User_Driver.csv User
 .import test_data/Customer.csv Customer
 .import test_data/Restaurant.csv Restaurant
